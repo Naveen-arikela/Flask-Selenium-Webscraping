c629df66cc1ab4b173595b891a2732f4bcdd98fd
naveenarike <naveenarike@digital-nirvana.com>
2024-01-11 21:32:22 +0530

    Added webdriver

--- web-scraping/Todo/selenium/test_webdriver.py
+++ web-scraping/Todo/selenium/test_webdriver.py
@@ -1,34 +1,34 @@
-from selenium import webdriver
-import time
-from flask import Flask, render_template, request
-import os
+# from selenium import webdriver
+# import time
+# from flask import Flask, render_template, request
+# import os
 
-app = Flask(__name__)
-root_dir = os.path.dirname(os.path.abspath(__file__))
-filename = f'saved_page_203.html'
-file_path = os.path.join(root_dir, filename)
+# app = Flask(__name__)
+# root_dir = os.path.dirname(os.path.abspath(__file__))
+# filename = f'saved_page_203.html'
+# file_path = os.path.join(root_dir, filename)
 
-url = 'https://www.thelancet.com/journals/laneur/article/PIIS1474-4422(22)00428-8/fulltext'
+# url = 'https://www.thelancet.com/journals/laneur/article/PIIS1474-4422(22)00428-8/fulltext'
 
-INPUT_FILES_DIRECTORY = 'input_files'
-os.makedirs(INPUT_FILES_DIRECTORY, exist_ok=True)
+# INPUT_FILES_DIRECTORY = 'input_files'
+# os.makedirs(INPUT_FILES_DIRECTORY, exist_ok=True)
 
-output_files_path = os.path.join(INPUT_FILES_DIRECTORY, 'test1258')
+# output_files_path = os.path.join(INPUT_FILES_DIRECTORY, 'test1258')
 
-for index in range(1):
-    driver = webdriver.Chrome()
-    driver.get(url)
+# for index in range(1):
+#     driver = webdriver.Chrome()
+#     driver.get(url)
 
-    #website html content
-    html_content = driver.page_source
-    filename = f'saved_page_{index}.html'
-    input_file_path = os.path.join(INPUT_FILES_DIRECTORY, filename)
-    with open(input_file_path, 'w', encoding='utf-8') as file:
-        file.write(html_content)
+#     #website html content
+#     html_content = driver.page_source
+#     filename = f'saved_page_{index}.html'
+#     input_file_path = os.path.join(INPUT_FILES_DIRECTORY, filename)
+#     with open(input_file_path, 'w', encoding='utf-8') as file:
+#         file.write(html_content)
 
-    # Close the browser window
-    time.sleep(10)
-    driver.quit()
+#     # Close the browser window
+#     time.sleep(10)
+#     driver.quit()
 
 
 
--- web-scraping/main/constants.py
+++ web-scraping/main/constants.py
@@ -2,7 +2,8 @@ import os
 
 #Form Types
 SCRAP_WEBSITE_FORM = "scrap-website-form"
-DISPLAY_TAGS_FORM = "show-tags-form"
+DISPLAY_TAGS_FORM = "display-tags-form"
+LOGIN_FORM = "login-form"
 
 #App Constants
 NO_CONTENT_RESPONSE = ('', 204)
@@ -11,7 +12,8 @@ FILE_EXTENSION = '.docx'
 
 #Tags
 SPECIAL_CONTAINER_TAGS = ['body']
-PARAGRAPH_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+PARAGRAPH_TAGS = ['div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+HEADER_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 TAGS_STRUCTURE = {
         'p': [{'h1': 'Title 1'}, {'p': 'Paragraph 1'}, {'h2': 'Title 2'}, {'h3': 'Title 3'}],
         'img': [{'img': 'image link'}]
@@ -29,3 +31,24 @@ USERNAME = 'admin'
 PASSWORD = 'admin@123'
 
 
+HTML_CONTENT = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Get Text from Div</title>
+</head>
+<body>
+
+<div id="myDiv">
+content 
+of div
+<p>this is the para2</p>
+data
+</div>
+<h1></h1>
+</body>
+</html>
+
+
+"""
